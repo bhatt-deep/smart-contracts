aada58ccd088291aae7d4cab923be200ff1a7103
Deep Bhatt <bhatt.deep.001@gmail.com>
2021-03-17 19:07:26 -0400

    Added functinalities in RBAC-2b

--- RBAC-2b.sol
+++ RBAC-2b.sol
@@ -19,27 +19,49 @@ library SafeMath {
 }
 
 interface IToken{
-    function transfer(address _receipient, uint256 __amount) external returns(bool);
+    function transfer(address _receipient, uint256 _amount) external returns(bool);
 
     function totalSupply() external returns(uint256);
 
-    function balanceOf(address _owner) external returns(uint256);
+    function balanceOf(address owner) external returns(uint256);
 }
 
-contract StandardToken {
+contract StandardToken is IToken {
     using SafeMath for uint256;
     
     // state variables
+    address public _owner;
     mapping (address => uint256) public  _balances;
     uint256 _totalSupply;
-    string memory _name;
-    string memory _symbol;
+    string _name;
+    string _symbol;
     
-    constructor() public{
-        _name = yorkcoin;
-        _symbol = yc;
-        _totalSupply = 10000;
+    constructor () public {
+        _owner = msg.sender;
+        _name = "yorkcoin";
+        _symbol = "yc";
+        _totalSupply = 1000;
     }
     
+    function mint(address owner, uint256 amount) public {
+        require(msg.sender == _owner, "Unauthorized");
+
+        _totalSupply = _totalSupply.add(amount);
+        _balances[owner] = _balances[owner].add(amount);
+    }
     
+    function transfer(address _receipient, uint256 _amount) public override returns (bool) {
+         require(msg.sender == _owner, "Unauthorized");
+        _balances[_owner] = _balances[_owner].sub(_amount);
+        _balances[_receipient] = _balances[_receipient].add(_amount);
+        return true;
+    }
+    
+    function balanceOf(address owner) public override returns (uint256) {
+        return _balances[owner];
+    }
+    
+    function totalSupply() public override returns (uint256) {
+        return _totalSupply;
+    }
 }
